0190d159ed2a73ba58cd720fcafa548cfad4b0ca
Ernando Braga <nandobraga78@gmail.com>
2022-07-16 15:19:15 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import NotifictionButton from "./components/NotifictionButton"
 
 
 function App() {
-return(
-  <h1>Olá mundo!</h1>
-)
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotifictionButton />
+      <NotifictionButton />
+     
+
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/components/NotifictionButton/index.tsx
@@ -0,0 +1,13 @@
+import butto from '../../assets/img/Notifiction-icon.svg'
+
+
+import './styles.css'
+function NotifictionButton() {
+  return (
+    <div className="dsmeta-red-btn">
+      <img src= {butto} alt="Notificar" />
+    </div>
+  )
+}
+
+export default NotifictionButton
